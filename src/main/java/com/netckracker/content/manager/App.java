b975900731824970190b4d5244a099c4890493c3
lizagorovets <liza.gorovets@yandex.ru>
2018-02-13 09:33:59 +0300

    Create application

---
+++ src/main/java/com/netckracker/content/manager/App.java
@@ -0,0 +1,13 @@
+package com.netckracker.content.manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
